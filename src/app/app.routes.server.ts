ac7bbd679109af06ac07df393a2063bf13ed13ed
CrisMen2610 <crisdamencast@gmail.com>
2025-09-08 05:21:32 -0500

    fix(routes): routes fixed

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,41 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'how-it-works',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { id: 'account-setup' },
+        { id: 'strategy-setup' },
+        { id: 'trading-psychology' },
+        { id: 'new-trade-switch' },
+        { id: 'trade-switch-vs-none' },
+        { id: 'chrome-extension' },
+      ];
+    },
+  },
+  {
+    path: 'faq',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'plans',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about-us',
+    renderMode: RenderMode.Prerender,
+  },
 ];
