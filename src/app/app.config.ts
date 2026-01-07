55e9e84b82c57ee848776308bd26619fd0bc421c
Juan Esteban Duque <mijuan79e8@gmail.com>
2026-01-07 01:52:22 +0100

    feat(router): implement in-memory scrolling for improved navigation experience; update styles in about-us and plans components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,12 +16,19 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { withInMemoryScrolling } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',  // siempre arriba
+        anchorScrolling: 'enabled',        // opcional, para hash links
+      })
+    ),
     provideAnimations(),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
